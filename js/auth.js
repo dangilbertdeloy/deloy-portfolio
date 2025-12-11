d7c09e74e6e1008a3bee8364223ba4b7b04bdd96
dangilbertdeloy <dangilbert.deloy@csucc.edu.ph>
2025-12-11 14:15:06 +0800

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,59 @@
+// Firebase imports
+import { 
+  initializeApp 
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCl5hlO_FK2Pl4un8S4rKbQNj9sYy5WLi0",
+  authDomain: "dreamscape-portfolio.firebaseapp.com",
+  projectId: "dreamscape-portfolio",
+  storageBucket: "dreamscape-portfolio.firebasestorage.app",
+  messagingSenderId: "1008209469846",
+  appId: "1:1008209469846:web:080c4b22e8eb1cd9923db7",
+  measurementId: "G-4CS4205RSF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// -----------------------------
+// REGISTER USER
+// -----------------------------
+export function registerUser(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+// -----------------------------
+// LOGIN USER
+// -----------------------------
+export function loginUser(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+// -----------------------------
+// LOGOUT USER
+// -----------------------------
+export function logoutUser() {
+  return signOut(auth);
+}
+
+// -----------------------------
+// PROTECT PORTFOLIO PAGE
+// -----------------------------
+export function protectPage() {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "login.html";
+    }
+  });
+}
